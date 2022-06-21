ed8a609dc4cd161c0a24498ee3b9036b656fa9d2
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-06-21 17:10:36 +0430

    Initial Commit

---
+++ Todo App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo App
+//
+//  Created by Amir Mohammad on 3/31/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Amir Mohammad on 3/31/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
