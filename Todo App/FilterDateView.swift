70245dbc427230e0b00ff90eeb240b77c6cc7832
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-06-21 20:43:24 +0430

    second tab added

---
+++ Todo App/FilterDateView.swift
@@ -0,0 +1,57 @@
+//
+//  FilterDateView.swift
+//  Todo App
+//
+//  Created by Amir Mohammad on 3/31/1401 AP.
+//
+
+import SwiftUI
+
+struct FilterDateView: View {
+
+    @Binding var items: [TodoItem]
+    @State var filteredItems: [TodoItem] = []
+    @State var date: Date
+    
+    init(items: Binding<[TodoItem]>) {
+        self._items = items
+        _date = State(initialValue: Date())
+    }
+    
+    func buttonTapped() {
+        for item in items {
+            if item.date == date {
+                filteredItems.append(item)
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 10) {
+                Form {
+                    DatePicker("Date", selection: $date)
+                }
+                List {
+                    ForEach(filteredItems, id: \.id) {
+                        item in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(item.name)
+                                .bold()
+                                .font(.title3)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Search todo'es by date ")
+            .toolbar {
+                Button {
+                    buttonTapped()
+                } label: {
+                    Text("Search")
+                }
+            }
+        }
+    }
+}
+
