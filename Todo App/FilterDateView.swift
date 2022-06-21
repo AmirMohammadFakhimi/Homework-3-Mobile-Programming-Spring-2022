a58ad8fa50c7d3333c74b1151faa8dda5a991845
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-06-21 23:56:10 +0430

    date picker

--- Todo App/FilterDateView.swift
+++ Todo App/FilterDateView.swift
@@ -10,36 +10,40 @@ import SwiftUI
 struct FilterDateView: View {
 
  @Binding var items: [TodoItem]
- @State var filteredItems: [TodoItem] = [] {
-    didSet {
-        buttonTapped()
-    }
-}
- @State var date: Date
+@State var filteredItems: [TodoItem] = []
+ @State var dueDate: Date
 
  init(items: Binding<[TodoItem]>) {
      self._items = items
-     _date = State(initialValue: Date())
+     _dueDate = State(initialValue: Date())
  }
 
  func buttonTapped() {
     for item in items {
-        if item.dueDate.getYear == date.getYear &&
-           item.dueDate.getMonth == date.getMonth &&
-           item.dueDate.getDay == date.getDay {
+        if item.dueDate.get(.day, .month, .year).year == dueDate.get(.day, .month, .year).year &&
+           item.dueDate.get(.day, .month, .year).month == dueDate.get(.day, .month, .year).month &&
+           item.dueDate.get(.day, .month, .year).day == dueDate.get(.day, .month, .year).day {
             filteredItems.append(item)
         }
     }
+     
 }
 
  var body: some View {
      NavigationView {
-         VStack(alignment: .leading, spacing: 10) {
-             Form {
-                 DatePicker("Date", selection: $date)
-             }
-             List {
-                 ForEach(filteredItems, id: \.ID) {
+         VStack(alignment: .leading) {
+            List {
+                DatePicker("Date", selection: $dueDate, displayedComponents: .date)
+                    .onChange(of: filteredItems) { value in
+                        value.dueDate.get(.day, .month, .year).year == dueDate.get(.day, .month, .year).year &&
+                           value.dueDate.get(.day, .month, .year).month == dueDate.get(.day, .month, .year).month &&
+                           value.dueDate.get(.day, .month, .year).day == dueDate.get(.day, .month, .year).day {
+                            filteredItems.append(value)
+                        }
+                    }
+                
+                Spacer()
+                ForEach(filteredItems, id: \.ID) {
                      item in
                      VStack(alignment: .leading, spacing: 10) {
                          Text(item.name)
@@ -49,7 +53,7 @@ struct FilterDateView: View {
                  }
              }
          }
-         .navigationTitle("Search todo'es by date ")
+         .navigationTitle("Search Todos by date")
          .toolbar {
              Button {
                  buttonTapped()
@@ -58,6 +62,16 @@ struct FilterDateView: View {
              }
          }
      }
+     
  }
 }
 
+extension Date {
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+}
